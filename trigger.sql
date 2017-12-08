1c7200f9961fbad22e4c6f89147c92f08e01a271
DarkLaion <laionel.lellis@outlook.com>
2017-12-07 22:24:17 -0200

    trigger histórico
    cadastra o histórico da reserva

---
+++ trigger.sql
@@ -0,0 +1,31 @@
+use srs;
+DELIMITER //
+CREATE TRIGGER tg_gravar_historico
+AFTER INSERT ON reserva FOR EACH ROW
+BEGIN
+    DECLARE varIdHistorico INT;
+    DECLARE varEstadoConservacao VARCHAR(255);
+
+    INSERT INTO historicoreservas
+        (idUsuario, idSala, dataReserva)
+    VALUES
+        (NEW.idUsuario, NEW.idSala, NEW.dataReserva);
+
+    SET varIdHistorico = (
+        SELECT MAX( idHistoricoReserva )
+        FROM   historicoreservas
+    );
+
+    SET varEstadoConservacao = (
+        SELECT estadoConservacao
+        FROM   sala
+        WHERE  idSala = NEW.idSala
+    );
+
+    UPDATE historicoreservas
+    SET    estadoConservacao = varEstadoConservacao
+    WHERE  idHistoricoReserva = varIdHistorico;
+END //;
+DELIMITER ;
+
+
